472efd7b769d8421faf8f63c49c3c0921c0e38fb
LeandrodeLimaC <leandrolima7472@gmail.com>
2021-06-22 21:13:49 -0300

    Created auth page styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,24 +2,25 @@ import illustrationSvg from '../assets/images/illustration.svg';
 import logoSvg from '../assets/images/logo.svg';
 import googleIconSvg from '../assets/images/google-icon.svg';
 
+import '../styles/auth.scss';
 
 export function Home() {
     return (
-        <div>
+        <div id="page-auth">
             <aside>
-                <img src={illustrationSvg} alt="Ilustração simbolizando perguntas e respostas" />
+                <img draggable="false" src={illustrationSvg} alt="Ilustração simbolizando perguntas e respostas" />
                 <strong>Crie salas de Q&amp;A ao vivo</strong>
                 <p>Tire as dúvidas da sua audiência em tempo-real</p>
             </aside>
             <main>
-                <div>
+                <div className="main-content">
                     <img src={logoSvg} alt="Logo LetMeAsk" />
-                    <button>
+                    <button className="create-room">
                         <img src={googleIconSvg} alt="Logo do Google" />
                         Crie sua sala com o Google
                     </button>
-                    <div>Ou entre em uma sala</div>
-                    <form action="">
+                    <div className="separator">ou entre em uma sala</div>
+                    <form>
                         <input
                             type="text"
                             placeholder="Digite o código da sala"
