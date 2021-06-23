289fe983429732a4bd8413ee103f8a52fad0d4b6
LeandrodeLimaC <leandrolima7472@gmail.com>
2021-06-22 23:40:44 -0300

    Removed all AuthContext related stuff, imported AuthContextProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,19 @@
-import { createContext, useState, useEffect } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
-import { firebase, auth } from './services/firebase'
+
+import { AuthContextProvider } from './contexts/AuthContext';
 
 import './styles/global.scss'
 
-type User = {
-  id: string;
-  name: string;
-  photo: string;
-}
-
-type AuthContextType = {
-  user: User | undefined;
-  signInWithGoogle: () => Promise<void>;
-}
-
-export const AuthContext = createContext({} as AuthContextType)
-
 function App() {
-  const [user, setUser] = useState<User>()
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user => {
-      if (user) {
-        const { displayName, photoURL, uid } = user;
-
-        if (!displayName || !photoURL) {
-          throw new Error('Missing informations from Google Account');
-        }
-
-        setUser({
-          id: uid,
-          name: displayName,
-          photo: photoURL
-        })
-      }
-    })
-
-    return () => {
-      unsubscribe();
-    }
-  }, [])
-
-  async function signInWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    const { user } = await auth.signInWithPopup(provider);
-
-    if (user) {
-      const { displayName, photoURL, uid } = user;
-
-      if (!displayName || !photoURL) {
-        throw new Error('Missing informations from Google Account');
-      }
-
-      setUser({
-        id: uid,
-        name: displayName,
-        photo: photoURL
-      })
-    }
-
-  }
-
   return (
     <BrowserRouter>
-      <AuthContext.Provider value={{ user, signInWithGoogle }}>
+      <AuthContextProvider>
         <Route path="/" exact component={Home} />
         <Route path="/rooms/new" component={NewRoom} />
-      </AuthContext.Provider>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
