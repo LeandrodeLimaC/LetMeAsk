8e5064f54bb230066b7171dc2b9b81cbbb8d19ac
LeandrodeLimaC <leandrolima7472@gmail.com>
2021-06-22 21:21:47 -0300

    Creating trully transparent wrapper button and styles for button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,11 @@
-import { useState } from "react"
+import { ButtonHTMLAttributes } from "react"
 
-export function Button() {
-    const [count, setCount] = useState(0)
+import '../styles/button.scss'
 
-    function increment() {
-        setCount(count + 1)
-    }
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
 
+export function Button(props: ButtonProps) {
     return (
-        <button onClick={increment}>Você clicou aqui {count}x</button>
+        <button className="button" {...props} />
     )
 }
