5a9aa1b5f9091605132b565679c9345b2b53df05
LeandrodeLimaC <leandrolima7472@gmail.com>
2021-06-22 23:42:58 -0300

    Fix indentation to 2 spaces

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,43 +11,43 @@ import { Button } from '../components/Button';
 import { AuthContext } from '../contexts/AuthContext';
 
 export function Home() {
-    const history = useHistory()
-    const { user, signInWithGoogle } = useContext(AuthContext)
+  const history = useHistory()
+  const { user, signInWithGoogle } = useContext(AuthContext)
 
-    async function handleCreateRoom() {
-        if (!user) {
-            await signInWithGoogle()
-        }
-
-        history.push('/rooms/new')
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle()
     }
 
-    return (
-        <div id="page-auth">
-            <aside>
-                <img draggable="false" src={illustrationSvg} alt="Ilustração simbolizando perguntas e respostas" />
-                <strong>Crie salas de Q&amp;A ao vivo</strong>
-                <p>Tire as dúvidas da sua audiência em tempo-real</p>
-            </aside>
-            <main>
-                <div className="main-content">
-                    <img src={logoSvg} alt="Logo LetMeAsk" />
-                    <button onClick={handleCreateRoom} className="create-room">
-                        <img src={googleIconSvg} alt="Logo do Google" />
-                        Crie sua sala com o Google
-                    </button>
-                    <div className="separator">ou entre em uma sala</div>
-                    <form>
-                        <input
-                            type="text"
-                            placeholder="Digite o código da sala"
-                        />
-                        <Button type="submit">
-                            Entrar na sala
-                        </Button>
-                    </form>
-                </div>
-            </main>
+    history.push('/rooms/new')
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img draggable="false" src={illustrationSvg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoSvg} alt="Logo LetMeAsk" />
+          <button onClick={handleCreateRoom} className="create-room">
+            <img src={googleIconSvg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+          <div className="separator">ou entre em uma sala</div>
+          <form>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+            />
+            <Button type="submit">
+              Entrar na sala
+            </Button>
+          </form>
         </div>
-    )
+      </main>
+    </div>
+  )
 }
