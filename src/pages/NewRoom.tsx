dc321440266197892ce2d8d385d5eb1681592a90
LeandrodeLimaC <leandrolima7472@gmail.com>
2021-06-22 23:42:58 -0300

    Fix indentation to 2 spaces

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -8,29 +8,29 @@ import '../styles/auth.scss';
 import { Button } from '../components/Button';
 
 export function NewRoom() {
-    return (
-        <div id="page-auth">
-            <aside>
-                <img draggable="false" src={illustrationSvg} alt="Ilustração simbolizando perguntas e respostas" />
-                <strong>Crie salas de Q&amp;A ao vivo</strong>
-                <p>Tire as dúvidas da sua audiência em tempo-real</p>
-            </aside>
-            <main>
-                <div className="main-content">
-                    <img src={logoSvg} alt="Logo LetMeAsk" />
-                    <h2> Criar uma nova sala </h2>
-                    <form>
-                        <input
-                            type="text"
-                            placeholder="Nome da sala"
-                        />
-                        <Button type="submit">
-                            Criar na sala
-                        </Button>
-                    </form>
-                    <p>Quer entrar em uma sala existente? <Link to="/">clique aqui</Link></p>
-                </div>
-            </main>
+  return (
+    <div id="page-auth">
+      <aside>
+        <img draggable="false" src={illustrationSvg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoSvg} alt="Logo LetMeAsk" />
+          <h2> Criar uma nova sala </h2>
+          <form>
+            <input
+              type="text"
+              placeholder="Nome da sala"
+            />
+            <Button type="submit">
+              Criar na sala
+            </Button>
+          </form>
+          <p>Quer entrar em uma sala existente? <Link to="/">clique aqui</Link></p>
         </div>
-    )
+      </main>
+    </div>
+  )
 }
