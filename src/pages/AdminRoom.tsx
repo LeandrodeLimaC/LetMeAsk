e47584b0e0233bba5f3c695cce938e7d7f79079c
LeandrodeLimaC <leandrolima7472@gmail.com>
2021-06-24 22:52:26 -0300

    Removed unecessary handlers

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -7,9 +7,7 @@ import { Button } from '../components/Button';
 import { Question } from '../components/Question';
 import { RoomCode } from '../components/RoomCode';
 
-import { useAuth } from '../hooks/useAuth';
 import { useRoom } from '../hooks/useRoom';
-import { database } from '../services/firebase';
 
 import '../styles/room.scss'
 
@@ -18,34 +16,10 @@ type RoomParams = {
 }
 
 export function AdminRoom() {
-  const { user } = useAuth()
+  // const { user } = useAuth()
   const params = useParams<RoomParams>();
-  const [newQuestion, setNewQuestion] = useState('');
   const roomId = params.id
   const { questions, title } = useRoom(roomId)
-
-  async function handleSendQuestion(event: FormEvent) {
-    event.preventDefault();
-
-    if (!newQuestion.trim()) return;
-
-    if (!user) {
-      throw new Error('You must be logged in');
-    }
-
-    const question = {
-      content: newQuestion,
-      author: {
-        name: user?.name,
-        avatar: user?.photo
-      },
-      isHighLighted: false,
-      isAnswered: false
-    };
-
-    await database.ref(`rooms/${roomId}/questions`).push(question)
-    setNewQuestion('')
-  }
 
   return (
     <div id="page-room">
