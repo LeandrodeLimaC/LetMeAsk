4c0dfea7ec563f968c5b75180089817325809558
LeandrodeLimaC <leandrolima7472@gmail.com>
2021-06-26 15:26:35 -0300

    Separated components files

--- src/components/RoomCode/index.tsx
+++ src/components/RoomCode/index.tsx
@@ -1,6 +1,6 @@
-import copySvg from '../assets/images/copy.svg';
+import copySvg from '../../assets/images/copy.svg';
 
-import '../styles/room-code.scss';
+import './styles.scss';
 
 type RoomCodeProps = {
   code: string
